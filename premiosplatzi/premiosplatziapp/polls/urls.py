0cc1c44062c1b32fbedf398bdb9bd289f94cff03
TakahiroKz <KizugaTakahiro@gmail.com>
2023-05-23 19:53:31 -0500

    Se mejora los templates, el index, urls y views para evitar hardcoding

--- premiosplatzi/premiosplatziapp/polls/urls.py
+++ premiosplatzi/premiosplatziapp/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #ex: /polls/5/
     path("<int:question_id>/detail/", views.detail,name="detail"),
     #ex: /polls/5/results
-    path("<int:question_id>/", views.results,name="results"),
+    path("<int:question_id>/results/", views.results,name="results"),
     #ex: /polls/5/vote
-    path("<int:question_id>/", views.vote,name="votes")
+    path("<int:question_id>/vote/", views.vote,name="vote")
 ]

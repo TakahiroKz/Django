70260b72205e6613fc52a4c08eae9a887e592566
TakahiroKz <KizugaTakahiro@gmail.com>
2023-06-05 13:49:30 -0500

    Actualizacion

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -11,14 +11,16 @@ def index(request):
         "latest_question_list": latest_question_list
     })
 
-
 def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request,"polls/detail.html", {"question":question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html',{
+        "question":question
+    })
 
 
 def vote(request, question_id):
